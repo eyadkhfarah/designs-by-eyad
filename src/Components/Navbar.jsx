c99855408fd1a839bd7ccd3c93984fefab57a5e4
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2023-04-25 21:31:57 +0200

    navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 
 
 export default function Navbar() {
-  return (
-    <nav className="flex px-24 justify-between p-5">
-        Navbar
-        {NavList.map((nav) => (
-            <li key={nav.id}>
-                <Link href={nav.link}><ul>{nav.name}</ul></Link>
+    return (
+        <nav className="flex px-24 justify-between p-5">
+            <span>Navbar</span>
+            <li>
+                {NavList.map((nav) => (
+                    <Link href={nav.link}><ul key={nav.id}>{nav.name}</ul></Link>
+                ))}
             </li>
-        ))}
-    </nav>
-  )
+        </nav>
+    )
 }
