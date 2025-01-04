499cc7986aba368e8921122ba67792e42c5770c0
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2025-01-04 22:18:39 +0200

    update puchasing info

--- src/app/contact/purchasing/page.tsx
+++ src/app/contact/purchasing/page.tsx
@@ -22,26 +22,46 @@ export default function page() {
     <>
       <h2 className="uppercase text-xl">Purchasing Methods</h2>
       <div className="grid lg:grid-cols-2 lg:gap-6 gap-3 my-10">
-        {/* <a
+        <a
           href="tel:+201555715783"
           target="_blank"
-          rel="noopener noreferrer"
-          className="w-full p-8 bg-gray-900 font-bold rounded-3xl uppercase grid gap-6"
-        >
-          <div>
-            <h3>Phone Cash</h3>
-          </div>
-        </a> */}
-        <div className="w-full p-8 bg-gray-900 font-bold rounded-3xl uppercase grid gap-6">
-          <h3>InstaPAy</h3>
+          rel="noopener noreferrer" className="w-full lg:grid hidden p-8 bg-gray-900 font-bold rounded-3xl uppercase gap-6">
+
+          <h3>NBE Phone Cash</h3>
           <Image
             alt="Instapay QR Code"
-            src={"/instapay-qr-code.svg"}
+            src={"/Imgs/Purchasing/nbe-cash.png"}
             width={400}
             height={400}
             className="rounded-3xl"
           />
-        </div>
+        </a>
+        <a
+          href="https://ipn.eg/S/eyadfarah/instapay/35P7cS"
+          target="_blank"
+          rel="noopener noreferrer" className="w-full lg:grid hidden p-8 bg-gray-900 font-bold rounded-3xl uppercase gap-6">
+          <h3>InstaPAy</h3>
+          <Image
+            alt="Instapay QR Code"
+            src={"/Imgs/Purchasing/instapay-qr-code.png"}
+            width={400}
+            height={400}
+            className="rounded-3xl"
+          />
+        </a>
+        <a
+          href="ipn://S/eyadfarah/instapay/35P7cS"
+          target="_blank"
+          rel="noopener noreferrer" className="w-full p-8 lg:hidden bg-gray-900 font-bold rounded-3xl uppercase grid gap-6">
+          <h3>InstaPAy</h3>
+          <Image
+            alt="Instapay QR Code"
+            src={"/Imgs/Purchasing/instapay-qr-code.png"}
+            width={400}
+            height={400}
+            className="rounded-3xl"
+          />
+        </a>
       </div>
     </>
   );
