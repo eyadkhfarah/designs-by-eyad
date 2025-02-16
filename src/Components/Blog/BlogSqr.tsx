6c26328bba8731ce24cc738548ad053cfcff3f38
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2025-02-16 17:51:33 +0200

    Update BlogSqr.tsx

--- src/Components/Blog/BlogSqr.tsx
+++ src/Components/Blog/BlogSqr.tsx
@@ -9,7 +9,7 @@ export default function BlogSqr({ post }: NotionProps) {
         <Link className='blog-card' href={`/blog/${post.properties.Slug.rich_text[0].plain_text}`} rel="noreferrer">
             <Image className='rounded-2xl' alt={post.properties.Name.title[0].plain_text} src={`${post.properties.Thumbnail.files[0].name}`} width={1200} height={900} />
             <div className="grid gap-2">
-                <h2 className='text-2xl'>{post.properties.Name.title[0].plain_text}</h2>
+                <h2 className='text-2xl line-clamp-2'>{post.properties.Name.title[0].plain_text}</h2>
                 <p className='text-neutral-500 line-clamp-3'>{post.properties.Subtitle.rich_text[0].plain_text}</p>
             </div>
         </Link>
