8ae773014cb5561ab87136d253dd57a7e99671b2
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2025-02-22 03:50:10 +0200

    Update page.tsx

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,44 +1,43 @@
-// import ProtImages from "@/Components/Portfolio/ProtImages";
 import BlogSqr from "@/Components/Blog/BlogSqr";
 import { fetchPosts } from "@/lib/notion";
 import { Metadata } from "next";
-import React from "react";
 
 const siteUrl = process.env.PUBLIC_DOMAIN_URL || "https://designs-by-eyad.vercel.app";
 
 const meta = {
-    title: "Blog",
-    description: "Discover expert tips, creative insights, and design trends on the Designs By Eyad blog. Explore articles on brand identity, social media strategies, web design, and more to elevate your creative journey.",
-    url: "/blog",
-}
-
-export const metadata: Metadata = {
-    title: meta.title,
-    description: meta.description,
-    alternates: {
-        canonical: meta.url,
-    },
-    openGraph: {
-        title: meta.title,
-        description: meta.description,
-        url: meta.url,
-    },
+  title: "Blog",
+  description:
+    "Discover expert tips, creative insights, and design trends on the Designs By Eyad blog. Explore articles on brand identity, social media strategies, web design, and more to elevate your creative journey.",
+  url: `${siteUrl}/blog`,
 };
 
+export const metadata: Metadata = {
+  title: meta.title,
+  description: meta.description,
+  alternates: {
+    canonical: meta.url,
+  },
+  openGraph: {
+    title: meta.title,
+    description: meta.description,
+    url: meta.url,
+  },
+};
+
+// Revalidate this page every 1 second
+export const revalidate = 1;
+
 export default async function BlogsPage() {
-    const post = await fetchPosts()
+  const posts = await fetchPosts();
 
-    return (
-        <>
-            <section>
-                <h1>Blog</h1>
-
-                <div className="grid lg:grid-cols-3 gap-8">
-                    {post.results.map((post: any) => (
-                        <BlogSqr key={post.id} post={post} />
-                    ))}
-                </div>
-            </section>
-        </>
-    )
+  return (
+    <section>
+      <h1>Blog</h1>
+      <div className="grid lg:grid-cols-3 gap-8">
+        {posts.results.map((post: any) => (
+          <BlogSqr key={post.id} post={post} />
+        ))}
+      </div>
+    </section>
+  );
 }
