6b7d429a6d513d4edace164f2c393130ccea7198
Eyad Farah <45307338+eyadkhfarah@users.noreply.github.com>
2025-03-03 06:43:41 +0200

    Update robots.tsx

--- src/app/robots.tsx
+++ src/app/robots.tsx
@@ -10,6 +10,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/unused/',
     },
-    sitemap: siteUrl + 'sitemap.xml',
+    sitemap: siteUrl + '/sitemap.xml',
   }
 }
